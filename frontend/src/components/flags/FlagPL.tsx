547d4d08ee6f69bc7c7321c666379cd59e6d5789
Tomy1290 <79998921+Tomy1290@users.noreply.github.com>
2025-09-22 14:14:25 +0200

    Update FlagPL.tsx

--- frontend/src/components/flags/FlagPL.tsx
+++ frontend/src/components/flags/FlagPL.tsx
@@ -7,7 +7,7 @@ export default function FlagPL({ width = 36, height = 24, radius = 4 }: { width?
   return (
     <View style={{ width, height }}>
       <Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}> 
-        <Rect x={0} y={0} width={width} height={height/2} fill="#FFFFFF" rx={radius} ry={radius} />
+        <Rect x={0} y={0} width={width} height={height/2} fill="#FFFFFF"/>
         <Rect x={0} y={height/2} width={width} height={height/2} fill="#DC143C" />
       </Svg>
     </View>
