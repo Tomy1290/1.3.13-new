6aec09c33a8b7f9e75678ac7c1d99f09361e6668
Tomy1290 <79998921+Tomy1290@users.noreply.github.com>
2025-09-22 14:13:40 +0200

    Update FlagDE.tsx

--- frontend/src/components/flags/FlagDE.tsx
+++ frontend/src/components/flags/FlagDE.tsx
@@ -6,7 +6,7 @@ export default function FlagDE({ width = 36, height = 24, radius = 4 }: { width?
   return (
     <View style={{ width, height }}>
       <Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}> 
-        <Rect x={0} y={0} width={width} height={height/3} fill="#000" rx={radius} ry={radius} />
+        <Rect x={0} y={0} width={width} height={height/3} fill="#000"/>
         <Rect x={0} y={height/3} width={width} height={height/3} fill="#DD0000" />
         <Rect x={0} y={2*height/3} width={width} height={height/3} fill="#FFCE00" />
       </Svg>
